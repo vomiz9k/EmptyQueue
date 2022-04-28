2f586f952390dbb20f1a097c6c9211ae501a8ece
vomiz9k <kirill@kyazimov.ru>
2022-04-29 02:45:18 +0300

    2

--- src/main/java/com/emptyqueue/backend/BackendApplication.java
+++ src/main/java/com/emptyqueue/backend/BackendApplication.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @SpringBootApplication
-@CrossOrigin(origins = "http://localhost:3000", methods = {RequestMethod.OPTIONS, RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE}, allowedHeaders = "*", allowCredentials = "true")
+@CrossOrigin(origins = "http://localhost:8228", methods = {RequestMethod.OPTIONS, RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE}, allowedHeaders = "*", allowCredentials = "true")
 public class BackendApplication {
     public static void main(String[] args) {
         SpringApplication.run(BackendApplication.class, args);
